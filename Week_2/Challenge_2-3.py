2f4ecf5cbf3b068577310124a6dd0c344a7424d3
Tolulope <tbones4665@gmail.com>
2019-08-13 15:04:31 +0100

    Revert "Split Programs into `for` and `while` loops"
    This reverts commit ca668e94da32b2fc8439ca35e4a9eb79c64969da.

--- Week_2/Challenge_2-3.py
+++ Week_2/Challenge_2-3.py
@@ -1,43 +1,19 @@
-# Given: Two positive integers a < b <10000
-# Return: The sum of all odd integers from a through b inclusively. (Solve using
-# for loops
-
-a = int(input("Input Number 1: "))
-b = int(input("Input Number 2: "))
-
-
-def sum_odd_for(a, b):
+def sum_odd(a, b):
     output = 0
-    if 0 < a < b < 10000:
-        for number in range(a, (b + 1)):
+    while 0 < a < b < 10000:
+        for number in range(a, (b+1)):
             if number % 2 == 1:
                 output += number
         return output
-    elif a > b:
-        return "Number A should not be greater than Number B"
-    elif a < 0:
-        return "Number A should be a positive integer"
-    elif b > 10000:
-        return "Number A or B should not be greater than 10,000"
+    else:
+        if a > b:
+            return "Number A should not be greater than Number B"
+        elif a or b < 0:
+            return "Number A or B should be a positive integer"
+        elif a or b > 10000:
+            return "Number A or B should not be greater than 10,000"
+        else:
+            return "Error in input. Please check"
 
 
-# Given: Two positive integers a < b <10000
-# Return: The sum of all odd integers from a through b inclusively. (Solve using
-# while loops
-
-
-def sum_odd_while(a, b):
-    result = 0
-    b += 1
-    while 0 < a < b < 10000:
-        if 1 == 1:
-            number = a
-            if number % 2 == 1:
-                result += number
-            a += 1
-        return result
-
-
-print(f'For Loop = {sum_odd_for(a, b)}')
-print(f'While Loop = {sum_odd_while(a, b)}')
-
+print(sum_odd(int(input("Input Number A: ")), int(input("Input Number B: "))))
